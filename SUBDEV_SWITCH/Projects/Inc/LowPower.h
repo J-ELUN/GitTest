208d844fe8ce6b30184599485f247fdd2a1a3051
JG <rockjg0917@163.com>
2017-07-28 13:41:02 +0800

    deleted SUBDEV_SWITCH

--- SUBDEV_SWITCH/Projects/Inc/LowPower.h
+++
@@ -1,9 +0,0 @@
-#ifndef __LOWPOWER_H__
-#define __LOWPOWER_H__
-#include "stm32l0xx_hal.h"
-
-void LowPower_Init(void);
-void Enter_LowPower_Mode(void);
-void SystemClockConfig_STOP(void);
-
-#endif
